9ecefbd65ba75ebcebaa4e9f5157eba3932ae254
Shuvrajit <shuvrajitdebroy.official@gmail.com>
2024-08-27 00:58:14 +0530

    added BestDealFinder

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
+
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center"> 
+            <p className="small-text">
+              Smart Shopping Starts Here
+            </p>
+
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> DealFinder</span>
+            </h1>
+
+            <p className="mt-6">
+            Track Prices, Save Smart—Your Best Online Deals, Simplified.<br/>
+            The ultimate destination for your next online shopping addiction.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
